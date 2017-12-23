83cd6efae9511d9b4a18b0b4a8a2568304efa34f
IamTechnotron <technotron.avik@gmail.com>
2017-12-23 14:19:49 +0530

    remove the concept of valid word - make all words valid, even though not in the database

--- py_files/dictionary.py
+++ py_files/dictionary.py
@@ -6,8 +6,7 @@ def word_input():
     word = input("Enter the word to search: ")
     db = database.create_db(database.resolve_path(), word[0].lower())
     sorted(db)
-    if not valid_word(word):
-        print("ERROR: Not a proper word")
+
     if type(db) == dict:
         for key in db:
 
@@ -16,19 +15,6 @@ def word_input():
                 continue
     '''else:
         print('ERROR: Not a valid word or Word not in Database ')'''
-
-
-def valid_word(word):
-
-    # checking or a word starting with a special character or number
-    if ord(word[0].upper()) not in range(65, 93):
-        return 0
-
-    # checking for a phrase
-    if ' ' in word:
-        return 0
-
-    return 1
 
 
 # Test Case:
