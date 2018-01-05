885335b922fc340f6cf948a413ad4dd2a85c4851
IamTechnotron <technotron.avik@gmail.com>
2018-01-06 01:51:14 +0530

    include 'add word' feature and abbreaviations viewer - create a new add_word function - create a new dictionary to store abbreviations with   meanings and a function to print them -

--- py_files/dictionary.py
+++ py_files/dictionary.py
@@ -1,12 +1,14 @@
-from os import path
 from py_files import database
+
+
+abb_table = {"adj.": "\t: adjective", "abb.": "\t: abbreviation",  "adv.": "\t: adverb", "conj.": "\t: conjunction",
+             "n.": "\t\t: noun", "pl.": "\t: plural", "p. p.": "\t: prepositional phrase", "prep.": "\t: preposition",
+             "v.": "\t\t: verb", "v. t.": "\t: transitive verb"}
 
 
 def word_input():
     word = input("Enter the word to search: ")
     db = database.create_db(word[0].lower())
-    #sorted(db)
-
     if type(db) == dict:
         for key in db:
 
@@ -17,5 +19,46 @@ def word_input():
         print('ERROR: Not a valid word or Word not in Database ')'''
 
 
-# Test Case:
-word_input()
+def add_word():
+    """
+    Creates a wizard to add a word as per user.
+    First it takes the word,
+    then its part-of-speech type
+    and finally the meaning.
+    (Wizard aborts on faulty parts-of-speech type)
+    Lastly it confirms the action and on positive reply adds to word in the database as per lexographical order!
+    :return: None
+    """
+    word = input("Enter the word: ")
+    print("Enter the abbreviated type/parts-of-speech of the word if any or just skip if nothing special: ")
+    print_abbreviation_reference()
+
+    word_type = input("\n>>> ")
+    if word_type:
+        if word_type not in abb_table:
+            print("Not a known parts-of-speech! Aborting the wizard!")
+            return
+    word_type = '(' + word_type + ')'
+
+    meaning = input("Enter Meaning: ")
+    print("You are entering the following to the database!")
+    print(word, word_type, meaning)
+
+    choice = input("This action is IRREVERSIBLE!\nPLEASE CONFIRM WITH 'Y' FOR YES OR 'N' FOR NO\n>>> ")
+    if choice is 'Y' or choice is 'y':
+        print("The word has been added to the database!")
+        # please add the word
+    else:
+        print("Word adding wizard has the stopped! WORD NOT ADDED!")
+    return
+
+
+def print_abbreviation_reference():
+    # create a table here and print it
+    for key in abb_table.keys():
+        print(key, abb_table[key])
+
+
+# Test Cases:
+# add_word()
+# word_input()
