8a63eab2fd9e58cb1047b13aff99f9f3606c34bb
IamTechnotron <technotron.avik@gmail.com>
2018-01-06 15:17:41 +0530

    create main function in parent directory to call others

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from py_files import resolve
+
+
+def main():
+    print("\n\nWELCOME TO TERMINAL DICTIONARY\n\n")
+    resolve.word_input()
+
+
+if __name__ == "__main__":
+    main()
+
